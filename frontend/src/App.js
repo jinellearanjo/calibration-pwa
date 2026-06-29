6a63c6b5f14a5f5fe0eb4653a5dd5fced1cfb10f
jinellearanjo <f20240024@dubai.bits-pilani.ac.in>
2026-06-29 14:45:05 +0400

    configure CORS and test frontend-backend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/health")
+      .then(r => r.json())
+      .then(data => console.log(data));
+  }, []);
+
+  return <div>Calibration App</div>;
 }
 
 export default App;
